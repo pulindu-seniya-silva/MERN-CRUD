4fcf6cbb9ecd96c4cd4ed1613d0e7b22d94ae689
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-02-07 10:19:06 +0530

    started to develop server

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,41 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//creating the server
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import logger from "./src/utils/logger.js"
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const PORT = process.env.PORT || "8090";
 
-var app = express();
+app.use(cors());
+app.use(express.json({ limit: "20mb"}));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get("/", (re1, res, next) => {
+  res.send("<h2>📖 Library Management System API<h2>")
+  next();
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(PORT, () => {
+  logger.info("This is testing");
+  console.log(`🚀 Server is up and running on PORT ${PORT}`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
